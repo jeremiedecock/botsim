6020cc1dcb60e2be251ee2e2c15ebcd819de80b0
Jeremie DECOCK <gremy@tuxfamily.org>
2011-02-17 16:48:02 +0100

    Remove useless files form Bullet library

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 find src -name "*.cpp" -execdir gcc -I/home/gremy/git_pub/botsim/include/bullet/src/ -c {} \;
-find src -name "*.o"
+gcc -o botsim $(find src -name "*.o") $(pkg-config --libs openscenegraph) -lm lib/libbulletdynamics.a lib/libbulletcollision.a lib/libbulletmath.a
