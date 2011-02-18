296a86142e86f024126eb19dbd76f5185c55caf4
Jeremie DECOCK <gremy@tuxfamily.org>
2011-02-18 14:21:20 +0100

    Move bullet headers in './include/bullet/'

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-find src -name "*.cpp" -execdir gcc -I/home/gremy/git_pub/botsim/include/bullet/src/ -c {} \;
+find src -name "*.cpp" -execdir gcc -I/home/gremy/git_pub/botsim/include/bullet/ -c {} \;
 echo "Link"
 gcc -o botsim $(find src -name "*.o") $(pkg-config --libs openscenegraph) -losgShadow -lm lib/libbulletdynamics.a lib/libbulletcollision.a lib/libbulletmath.a
