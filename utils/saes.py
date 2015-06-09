ef7bed53902ed5c82e277ed3eded22e328ef0081
Jeremie DECOCK <jd.jdhp@gmail.com>
2015-06-10 00:46:49 +0200

    Prepare 'saes.py' for botsim (work in progress).

--- utils/saes.py
+++ utils/saes.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 """
 This is a simple Python implementation of the (mu/mu_I, lambda)-sigmaSA-ES
@@ -17,11 +17,14 @@ import random
 MU = 3                    # number of parents
 LAMBDA = 12               # number of offspring
 
-X_INIT = np.ones(1)       # initial parent vector 
+X_INIT = np.ones(24)       # initial parent vector 
 #X_INIT = np.ones(2)       # initial parent vector 
 SIGMA_INIT = 1            # initial global mutation strength sigma 
 
 SIGMA_MIN = 1e-10         # ES stops when sigma is smaller than sigma_min
+
+gen_index = 0
+indiv_index = 0
 
 ###########################################################
 
@@ -34,9 +37,30 @@ def sphere_function(indiv):
     return y
 
 def robudog(indiv):
-    pass
+    param_file_name = "gen" + str(gen_index) + "_indiv" + str(indiv_index)
+    score_file_name = param_file_name + ".score"
 
-fitness = sphere_function
+    score = None
+
+    # check the argument
+    x = indiv.x
+    assert x.shape == X_INIT.shape, x
+
+    # write params file
+    with open(param_file_name, "w") as fd:
+        for xi in x:
+            print(xi, end=' ', file=fd)
+
+    # run botsim.org
+    # TODO
+
+    # retrive the score
+    with open(score_file_name, "r") as fd:
+        score = float(fd.read())
+
+    return score
+
+fitness = robudog
 
 ###########################################################
 
@@ -72,6 +96,9 @@ def recombine_individuals(parents):
 
 def main():
 
+    global gen_index
+    global indiv_index
+
     # Initialization
     n = X_INIT.shape[0]         # determine search space dimensionality n   
     tau = 1. / math.sqrt(2.*n)  # self-adaptation learning rate
@@ -81,15 +108,24 @@ def main():
 
     # Evolution loop of the (mu/mu_I, lambda)-sigma-SA-ES
     while parent_pop[0].sigma > SIGMA_MIN:
+        gen_index += 1
+        print("Generation", gen_index)
+
         offspring_pop = []
         recombinant = recombine_individuals(parent_pop) # TODO: BUG ? this statement may be in the next line
         for i in range(1, LAMBDA):
+            indiv_index = i
+
             offspring_sigma = recombinant.sigma * math.exp(tau * random.normalvariate(0,1))
             offspring_y = recombinant.x + offspring_sigma * np.random.normal(size=n)
             offspring = Individual(offspring_y, offspring_sigma)
             offspring_pop.append(offspring)
+
+            print(" Individual", indiv_index, "score", offspring.reward)
         parent_pop = select_individuals(offspring_pop)
-        print(parent_pop[0])
+
+        print(" Best individual", parent_pop[0].x, "score", parent_pop[0].reward)
+        #print(parent_pop[0])
 
 # Remark: Final approximation of the optimizer is in "parent_pop[0].x"
 #         corresponding fitness is in "parent_pop[0].reward" and the final 
